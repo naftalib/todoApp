41713cfa3ad7e112d994bcb3920bf17edc4e78d9
naftalib <45719650+naftalib@users.noreply.github.com>
2020-11-22 10:13:19 +0200

    Updates

---
+++ precache-manifest.57b1b2fedd09cb308232996359edde46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0ab0beeaf5cfa55c935dd6b0569ce2",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "46512a5e3f4c8b68fad0",
+    "url": "/todoApp/static/css/main.aa6bdf9d.chunk.css"
+  },
+  {
+    "revision": "acc25bfc3f2b155df596",
+    "url": "/todoApp/static/js/2.406d338a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoApp/static/js/2.406d338a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46512a5e3f4c8b68fad0",
+    "url": "/todoApp/static/js/main.1cb98038.chunk.js"
+  },
+  {
+    "revision": "e0c216f817e4bfd98ea4",
+    "url": "/todoApp/static/js/runtime-main.b479ec2e.js"
+  }
+]);
